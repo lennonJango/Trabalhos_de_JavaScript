a76d1fc820571110beb395e1254c543a10bb6ca1
unknown <lennonjango@gmail.com>
2023-02-27 09:21:36 +0200

    Update

--- AJAX/country.js
+++ AJAX/country.js
@@ -1,6 +1,30 @@
 "use strict";
 const containerCountries = document.querySelector(".countries");
 
+const renderCountry = function (data, className = "") {
+  const currency_key = Object.keys(data.currencies)[0];
+  const current_languages = Object.keys(data.languages)[0];
+  //
+  const html = `<article class="country ">
+<img class="country__img" src="${data.flags.png}"/>
+ <div class=country__data ${className}">
+  <h3 class="country__name">${data.name.common}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+    2
+  )} people</p>
+  <p class="country__row"><span>🗣️</span>${
+    data.languages[current_languages]
+  }</p>
+  <p class="country__row"><span>💰</span>${
+    data.currencies[currency_key].name
+  }</p>
+</div>
+</article>`;
+  containerCountries.insertAdjacentHTML("beforeend", html);
+  containerCountries.style.opacity = 1;
+};
+
 function country(country) {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
@@ -9,31 +33,28 @@ function country(country) {
   request.addEventListener("load", function () {
     const [data] = JSON.parse(request.responseText);
     console.log(data);
-    // Desestruturar o Object para conseguir obter a moeda e a lingua do pais
-    const currency_key = Object.keys(data.currencies)[0];
-    const current_languages = Object.keys(data.languages)[0];
-    //
-    const html = `<article class="country">
-  <img class="country__img" src="${data.flags.png}"/>
-   <div class="country__data">
-    <h3 class="country__name">${data.name.common}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(2)}</p>
-    <p class="country__row"><span>🗣️</span>${
-      data.languages[current_languages]
-    }</p>
-    <p class="country__row"><span>💰</span>${
-      data.currencies[currency_key].symbol
-    }</p>
-  </div>
-</article>`;
 
-    containerCountries.insertAdjacentHTML("beforeend", html);
-    containerCountries.style.opacity = 1;
+    renderCountry(data);
+    // Esta parte do codigo e responsável por pegar os dados dos países vizinhos
+
+    const borders = data.borders;
+    for (let a in borders) {
+      if (a <= 1) {
+        const data1 = new XMLHttpRequest();
+        data1.open("GET", `https://restcountries.com/v3.1/alpha/${borders[a]}`);
+        data1.send();
+
+        data1.addEventListener("load", function () {
+          const [pais] = JSON.parse(data1.responseText);
+          console.log(pais);
+          renderCountry(pais);
+        });
+      }
+    }
   });
 }
 
-country("Chile");
-country("Mozambique");
+//country("Chile");
+country("Estados unidos");
+//country("Germany");
+//Vizinhos do pais
